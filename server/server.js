1eff6e1afbd4c4a1bf13653fbac1bb7ac6036a36
giuliojiang <giuliojiang@gmail.com>
2017-02-04 13:30:18 +0000

    static http server done

--- server/server.js
+++ server/server.js
@@ -1,3 +1,6 @@
+// #############################################################################
+// MAIL PART
+
 const nodemailer = require('nodemailer');
 var signer = require('nodemailer-dkim').signer;
 
@@ -18,15 +21,29 @@ transporter.use('stream', require('nodemailer-dkim').signer({
 // setup email data with unicode symbols
 let mailOptions = {
     from: '"Uni Secrets" <account@secrets.jstudios.ovh>', // sender address
-    to: 'test@test.com', // list of receivers
+    to: 'giuliojiang@gmail.com', // list of receivers
     subject: 'Uni Secrets account activation', // Subject line
     text: 'You activation code is 1234' // plain text body
 };
 
 // send mail with defined transport object
-transporter.sendMail(mailOptions, function(error, info) {
-    if (error) {
-        return console.log(error);
-    }
-    console.log('Message %s sent: %s', info.messageId, info.response);
+// transporter.sendMail(mailOptions, function(error, info) {
+//     if (error) {
+//         return console.log(error);
+//     }
+//     console.log('Message %s sent: %s', info.messageId, info.response);
+// });
+
+// #############################################################################
+// HTTP PART
+var express = require('express');
+var app = express();
+
+app.use('/', express.static(__dirname + '/../static'));
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!')
 });
+
+// #############################################################################
+// WEBSOCKET PART
