251504a262befe634fcf810ad0c5daa3ca8346e3
Giulio Jiang <giuliojiang@gmail.com>
2017-02-04 18:30:45 +0000

    fixing crashes due to half-open connections in http and websocket (#5)

--- server/http.js
+++ server/http.js
@@ -9,5 +9,8 @@ app.use('/', express.static(__dirname + '/../static'));
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
+}).on('error', function(err){
+    console.log('on error handler');
+    console.log(err);
 });
 
