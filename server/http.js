69e8e6d68f9ac90a5665268dfb2cdf8f6797ddcb
giuliojiang <giuliojiang@gmail.com>
2017-02-05 10:31:24 +0000

    setting up preprocessor

--- server/http.js
+++ server/http.js
@@ -5,7 +5,7 @@
 var express = require('express');
 var app = express();
 
-app.use('/', express.static(__dirname + '/../static'));
+app.use('/', express.static(__dirname + '/../tmp'));
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
