2685b7c509149263c7388dc106067f5cc961d732
giuliojiang <giuliojiang@gmail.com>
2017-02-22 15:59:20 +0000

    basic creation of nedb

--- server/db.js
+++ server/db.js
@@ -1,15 +1,64 @@
 // #############################################################################
 // DATABASE PART
-var mysql      = require('mysql');
 
-var pool = mysql.createPool({
-    host : 'localhost',
-    user : 'UniSecrets',
-    password : 'UniSecrets',
-    database: 'UniSecrets'
-});
+var Datastore = require('nedb');
+var path = require('path');
 
-module.exports = {
-    connection: pool
-};
+var db = {};
 
+/*
+USERS
+{
+    email
+    nickname
+    college
+    hash
+    activation
+    moderator?
+}
+*/
+db.users = new Datastore({
+    filename:  __dirname + '/../db/users.db',
+    autoload: true});
+db.users.persistence.setAutocompactionInterval(3600 * 1000);
+
+/*
+COLLEGES
+{
+    college
+    domain
+    active?
+}
+*/
+db.colleges = new Datastore({
+    filename:  __dirname + '/../db/colleges.db',
+    autoload: true});
+db.colleges.persistence.setAutocompactionInterval(3600 * 1000);
+
+/*
+POSTS
+{
+    postid
+    college
+    public
+    text
+    comments: [
+        {email, text}
+    ]
+    likes: [
+        {email}
+    ]
+    dislikes: [
+        {email}
+    ]
+    time
+}
+*/
+db.posts = new Datastore({
+    filename:  __dirname + '/../db/posts.db',
+    autoload: true});
+db.posts.persistence.setAutocompactionInterval(3600 * 1000);
+
+
+
+module.exports = db;
